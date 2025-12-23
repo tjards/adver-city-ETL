2695e0491132a51d81ad63e87ecadc22e7931c5f
tjards <p.jardine@queensu.ca>
2025-12-23 13:59:55 -0500

    extraction tool working

--- data_ingestion/utils.py
+++ data_ingestion/utils.py
@@ -2,6 +2,7 @@
 import os
 import requests
 from tqdm import tqdm
+import subprocess
 
 # build a filename from valid prefix, weather, and density
 def build_filename(prefix, weather, density, VALID_PREFIX, VALID_WEATHER, VALID_DENSITY):
@@ -48,3 +49,35 @@ def download_file(DATA_DIR, DOWNLOAD_URL, FILENAME):
             file.write(data)
     
     return FILE_PATH
+
+# extract a file format
+def extract_file(DIR_IN, FILENAME_IN, DIR_OUT, data_format="7z"):
+    
+    # build the file path to extract from 
+    FILE_PATH_IN = os.path.join(DIR_IN, FILENAME_IN)
+
+    # check if source file exists
+    if not os.path.exists(FILE_PATH_IN):
+        raise FileNotFoundError(f"File not found: {FILE_PATH_IN}")
+    
+    # make output directory if it doesn't exist
+    os.makedirs(DIR_OUT, exist_ok=True)
+
+    # safety: do not overwrite existing files
+    expected_subdir = os.path.splitext(FILENAME_IN)[0]      # clips the extension 
+    expected_dir = os.path.join(DIR_OUT, expected_subdir)   # builds expected directory 
+    if os.path.isdir(expected_dir):
+        print(f"Already extracted, skipping:\n  {expected_dir}")
+        return
+    
+    # extract using 7z
+    print(f"Extracting:\n  from : {FILE_PATH_IN}\n  to: {DIR_OUT}")
+
+    if data_format == "7z":
+        subprocess.run(
+            ["7z", "x", FILE_PATH_IN, f"-o{DIR_OUT}", "-y"], 
+            check=True
+        )
+    else:
+        raise ValueError(f"Trying to extract unsupported data format: {data_format}")
+
